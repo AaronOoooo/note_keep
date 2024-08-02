0af39f1db74d06345454ef51a7267ba150683092
Aaron Hall <aaron.o.hall@gmail.com>
2024-08-01 19:45:10 -0500

    Updated time-date format from 08-01-2024, 18:39 to Aug 1 2024, 6:39pm.

--- public/scripts.js
+++ public/scripts.js
@@ -57,7 +57,8 @@ async function loadNotes() {
 
 function formatDate(dateString) {
   const date = new Date(dateString);
-  return `${('0' + (date.getMonth() + 1)).slice(-2)}-${('0' + date.getDate()).slice(-2)}-${date.getFullYear()}, ${('0' + date.getHours()).slice(-2)}:${('0' + date.getMinutes()).slice(-2)}`;
+  const options = { month: 'short', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true };
+  return date.toLocaleString('en-US', options).replace(',', '');
 }
 
 async function editNote(id) {
